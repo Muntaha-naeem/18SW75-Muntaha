cd84bab58754323ca63bd854743e37b8522128df
Muntaha-naeem <49727845+Muntaha-naeem@users.noreply.github.com>
2019-04-17 22:01:03 +0500

    Add files via upload

---
+++ Lab tasks 1/MyInfo.java
@@ -0,0 +1,17 @@
+class MyInfo{
+	public static void main(String args[]){
+
+		string name = "Muntaha";
+		int totmarks = 83;
+		int perct = 83;
+		char grade = 'A';
+		string status = 'PASS';
+
+		System.out.println("NAME : " +name);
+		System.out.println("TOTAL MARKS : " +totmarks);
+		System.out.println("PERCENTAGE : " +perc+%);
+		System.out.println("GRADE : "  +grade);
+		System.out.println("STATUS : " +status);
+
+ }
+}
